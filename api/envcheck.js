cf2d1f9c4aeaae3a2c3c544e6ee0b55583acc7e6
zerkq8 <89047757+zerkq8@users.noreply.github.com>
2026-08-12 13:12:01 +0300

    Delete api/envcheck.js

--- api/envcheck.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('content-type', 'application/json');
-  res.status(200).end(JSON.stringify({
-    hasUrl: !!process.env.SUPABASE_URL,
-    hasKey: !!process.env.SUPABASE_KEY,
-    urlStart: (process.env.SUPABASE_URL || '').slice(0, 20),
-    keyLength: (process.env.SUPABASE_KEY || '').length
-  }));
-};
